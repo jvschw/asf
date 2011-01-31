fe59861f531b03e2983d973d178502233720ea9d
Jens Schwarzbach <jens.schwarzbach@unitn.it>
2011-01-31 19:37:07 +0100

    enhanced treatment of factor descriptions added entry for endRTonPage

--- code/ASF_readTrialDefs.m
+++ code/ASF_readTrialDefs.m
@@ -1,29 +1,77 @@
-function [trialdefs, factorinfo, errorflag, factorNames] = ASF_readTrialDefs(fname, Cfg)
-%function [trialdefs, factorinfo, errorflag, factorNames] = ASF_readTrialDefs(fname, Cfg)
+function [trialdefs, factorStruct, errorflag] = ASF_readTrialDefs(fname, Cfg)
+%function [trialdefs, factorStruct, errorflag] = ASF_readTrialDefs(fname, Cfg)
 %each line is composed of a list of entries, one line per trial
 %<code> [pageNumber_1, pageDuration_1]...[pageNumber_n, pageDuration_n] <startRTonPage>
 if ~isfield(Cfg, 'randomizeTrials'), Cfg.randomizeTrials = 0; else end;
 if ~isfield(Cfg, 'userDefinedSTMcolumns'), Cfg.userDefinedSTMcolumns = 0; else end;
-factorNames = {''};
+
+%CREATE AN EMPTY DESCRIPTION OF FACTORIAL STRUCTURE
+factorStruct.nFactors = [];
+factorStruct.factorLevels = [];
+factorStruct.factorNames = [];
+factorStruct.levelNames = [];
+
 errorflag = 0;
 if exist(fname, 'file') == 2
     %FILE EXISTS
     fid = fopen(fname);
     
-    %GET FACTOR LEVELS AND IF EXISTS FACTOR NAMES
+    %GET FACTOR LEVELS
     aline = fgetl(fid);
     tmpfactorinfo = textscan(aline, '%d');
-    nLevels = length(tmpfactorinfo{:});
-    factorinfo = double(tmpfactorinfo{:})';
+    factorStruct.factorLevels = double(tmpfactorinfo{:})';
+    factorStruct.nFactors = length(factorStruct.factorLevels);
+    
+    %CONVERT THE ENTIRE FACTORIAL INFO INTO A CELL 
     tmpfactorinfoX = textscan(aline, '%s');
     tmpfactorinfoX = tmpfactorinfoX{:};
-    if (length(tmpfactorinfoX)> nLevels)
-        %MORE ENTRIES THAN JUST NUMBERS, THUS WE HAVE TEXT CODING FACTOR NAMES
-        for i = 1:nLevels
-            factorNames{i} = tmpfactorinfoX{i+nLevels}; 
+    nEntries = length(tmpfactorinfoX);
+    %ERROR CHECKING
+    if (nEntries == 2*factorStruct.nFactors + sum(factorStruct.factorLevels))
+        %OK, WE HAVE A SITUATION LIKE THIS
+        % 2 2 congruence targetside congruent incongruent left right
+        %THE NUMERICAL DEsIGN-INFO [2, 2]
+        %THE FACTOR NAMES {'congruence', 'targetside'}
+        %NAMES FOR EACH LEVEL (PER FACTOR) 
+        %{congruent incongruent left right'}
+        %WE CAN PROCEED EXTRACTING RESPECTIVE NAMES
+        for i = 1:factorStruct.nFactors
+            factorStruct.factorNames{i} = tmpfactorinfoX{factorStruct.nFactors+i};
+            for j = 1:factorStruct.factorLevels(i)
+                factorStruct.levelNames{i, j} =...
+                    tmpfactorinfoX{2*factorStruct.nFactors + (i-1)*factorStruct.factorLevels(i) + j};
+            end
+        end
+        %WE END UP WITH SOMETHING LIKE THIS
+        %         factorStruct =
+        %
+        %          design: [2 2]
+        %        nFactors: 2
+        %     factorNames: {'congruence'  'targetside'}
+        %      levelNames: {2x2 cell}
+        
+    else
+        if (nEntries == factorStruct.nFactors)
+            %WE JUST HAVE THE INFO ABOUT NUMBER OF FACTORS AND RESPECTIVE
+            %LEVELS, WE CAN PROCEED (MAYBE WARN THE USER)
+        else
+            if (nEntries == factorStruct.nFactors*2)
+                %WE JUST HAVE THE INFO ABOUT NUMBER OF FACTORS AND THEIR
+                %RESPECTIVE NAMES (NO NAMES FOR FACTOR-LEVELS)
+                for i = 1:factorStruct.nFactors
+                    factorStruct.factorNames{i} =...
+                        tmpfactorinfoX{factorStruct.nFactors+i};
+                end
+                
+            else
+                %PRODUCE ERROR MESSAGE
+                errorflag = 1;
+            end
         end
     end
-
+    display(factorStruct)
+    
+    %READ IN TRIAL DEFINITIONS, ONE BY ONE
     counter = 0;
     while 1
         counter = counter + 1;
@@ -40,10 +88,26 @@ if exist(fname, 'file') == 2
                 trialdefs(counter).userDefined(c) = aline(2+c); %#ok<AGROW>
             end
         end
-        trialdefs(counter).pageNumber = aline(Cfg.userDefinedSTMcolumns+3:2:(end-2)); %#ok<AGROW>
-        trialdefs(counter).pageDuration = aline(Cfg.userDefinedSTMcolumns+4:2:(end-2)); %#ok<AGROW>
-        trialdefs(counter).startRTonPage = aline(end-1); %#ok<AGROW>
-        trialdefs(counter).correctResponse = aline(end); %#ok<AGROW>
+        
+        %THIS WILL ONLY STAY IN FOR A BRIEF PERIOD TO PROVIDE BACKWARD
+        %COMPATIBILITY WITH OLDER TRD FILES
+        if(Cfg.hasEndRTonPageInfo)
+            %VERSION THAT HAS endRTonPage
+            trialdefs(counter).pageNumber = aline(Cfg.userDefinedSTMcolumns+3:2:(end-3)); %#ok<AGROW>
+            trialdefs(counter).pageDuration = aline(Cfg.userDefinedSTMcolumns+4:2:(end-3)); %#ok<AGROW>
+            trialdefs(counter).startRTonPage = aline(end-2); %#ok<AGROW>
+            trialdefs(counter).endRTonPage = aline(end-1); %#ok<AGROW>
+            trialdefs(counter).correctResponse = aline(end); %#ok<AGROW>
+            
+        else
+            %VERSION THAT DOES NOT HAVE endRTonPage
+            trialdefs(counter).pageNumber = aline(Cfg.userDefinedSTMcolumns+3:2:(end-2)); %#ok<AGROW>
+            trialdefs(counter).pageDuration = aline(Cfg.userDefinedSTMcolumns+4:2:(end-2)); %#ok<AGROW>
+            trialdefs(counter).startRTonPage = aline(end-1); %#ok<AGROW>
+            trialdefs(counter).endRTonPage  = trialdefs(counter).startRTonPage;
+            trialdefs(counter).correctResponse = aline(end); %#ok<AGROW>
+        end
+        
     end
     fclose(fid);
 else
@@ -51,6 +115,10 @@ else
     errorflag = 1;
     error('FILE %s NOT FOUND', fname) 
 end
+
+%A LITTLE RANDOMIZATION PROCEDURE WHICH SHUFFLES TRIALS, ALLOWING FOR THE
+%RESTRICTION OF NO IMMEDIATE REPETITION OF THE SAME CONDITION
+%CONSIDER OUTSOURCING INTO ITS OWN FUNCTION
 if Cfg.randomizeTrials
     fprintf(1, 'RANDOMIZING TRIALS ... ');
     %RANDOMIZE ORDER OF TRIALS BUT NOT THE TRIAL ONSET VECTOR
